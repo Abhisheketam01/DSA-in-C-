678de9b9def998a5bdf99a73e34bb176efc2dec0
Abhisheketam01 <etamabhishek000@gmail.com>
2025-07-25 16:06:13 +0530

    prime.cpp

---
+++ Prime.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+
+int main() {
+    // 5. Print Prime Numbers from 1 to 100 Using Nested Loops
+    
+    int i;
+    int j;
+    int isPrime;
+
+    printf("Prime numbers between 1 and 100 are:\n");
+
+    for(i = 2; i <= 100; i++) {
+        isPrime = 1; // Assume number is prime
+
+        for(j = 2; j < i; j++) {
+            if(i % j == 0) {
+                isPrime = 0; // Not prime
+                break;
+            }
+        }
+
+        if(isPrime == 1)
+            printf("%d ", i);
+    }
+    
+    return 0;
+}
+
